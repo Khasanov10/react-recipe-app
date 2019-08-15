000d7957a5132fdf751ae70ecbcc86b5673cb893
Rasul <Khasanov1007@gmail.com>
2019-08-15 12:51:03 +0500

    finished

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
 export default class Recipe extends Component {
   render() {
     const {
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Recipe from "./Recipe";
-
 export default class RecipeList extends Component {
   render() {
     const { recipes } = this.props;
